47963823fb99cc62930f01fdb4bd1fcf04d5dd97
JinKyong <48237998+JinKyong@users.noreply.github.com>
2022-09-09 13:32:33 +0900

    first commit

---
+++ src/test/java/com/sparta/todayhouse/TodayHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.todayhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayHouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
