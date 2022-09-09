ce11f5a94c509971cc6ed2790296ce3975975a93
JinKyong <48237998+JinKyong@users.noreply.github.com>
2022-09-09 19:43:22 +0900

    securityConfiguration
    configuraion 추가

---
+++ src/main/java/com/sparta/todayhouse/configuration/SecurityConfiguration.java
@@ -0,0 +1,52 @@
+package com.sparta.todayhouse.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@ConditionalOnDefaultWebSecurity
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors();
+
+        http.csrf().disable()
+
+                //커스텀 예외처리
+                //.exceptionHandling()
+
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated();
+
+                //CustomFilter 적용(ex. jwtFilter)
+//                .and()
+//                .addFilterBefore()
+//                .addFilterAfter();
+
+        return http.build();
+    }
+}
