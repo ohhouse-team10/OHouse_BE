3c4edefe3091b966b44f2fb91ff63efb08772c6d
JinKyong <48237998+JinKyong@users.noreply.github.com>
2022-09-09 20:57:34 +0900

    CORS 설정

--- src/main/java/com/sparta/todayhouse/configuration/SecurityConfiguration.java
+++ src/main/java/com/sparta/todayhouse/configuration/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -40,6 +41,7 @@ public class SecurityConfiguration {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/test/**").permitAll()
+                .mvcMatchers(HttpMethod.OPTIONS, "/**").permitAll() //Preflight Request 허용해주기
                 .anyRequest().authenticated();
 
                 //CustomFilter 적용(ex. jwtFilter)
