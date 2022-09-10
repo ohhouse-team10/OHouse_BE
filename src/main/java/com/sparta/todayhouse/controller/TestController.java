95bb306e59b98fca5e47906d78243a7ec91d801b
JinKyong <48237998+JinKyong@users.noreply.github.com>
2022-09-10 23:53:47 +0900

    Update TestController.java

--- src/main/java/com/sparta/todayhouse/controller/TestController.java
+++ src/main/java/com/sparta/todayhouse/controller/TestController.java
@@ -9,19 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
-    public String testGet() { return "success_get"; }
+    public String testGet() { return "success_get!"; }
 
     @RequestMapping(value = "/post", method = RequestMethod.POST)
-    public String testPost() { return "success_post"; }
+    public String testPost() { return "success_post!"; }
 
     @RequestMapping(value = "/put", method = RequestMethod.PUT)
-    public String testPut() { return "success_put"; }
+    public String testPut() { return "success_put!"; }
 
     @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
-    public String testDelete() { return "success_delete"; }
+    public String testDelete() { return "success_delete!"; }
 
     @RequestMapping(value = "/customtest", method = RequestMethod.GET)
     public String customTest(){
-        return "custom test";
+        return "custom test!";
     }
 }
