a8137123013fd089119efcabc3522b7ef6cccc15
JinKyong <48237998+JinKyong@users.noreply.github.com>
2022-09-13 20:47:06 +0900

    controller return값 수정
    responseDtd -> responseEntity

--- src/main/java/com/sparta/todayhouse/controller/MemberController.java
+++ src/main/java/com/sparta/todayhouse/controller/MemberController.java
@@ -1,11 +1,13 @@
 package com.sparta.todayhouse.controller;
 
+import com.sparta.todayhouse.dto.ResponseMessage;
 import com.sparta.todayhouse.dto.request.LoginRequestDto;
 import com.sparta.todayhouse.dto.request.SignupRequestDto;
 import com.sparta.todayhouse.dto.response.MemberResponseDto;
 import com.sparta.todayhouse.dto.response.ResponseDto;
 import com.sparta.todayhouse.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,15 +20,16 @@ public class MemberController {
 
     private final MemberService memberService;
 
-//    @RequestMapping(value="/member/signup",method= RequestMethod.POST)
-    @PostMapping("/member/signup")
-    public ResponseDto<MemberResponseDto> signup(@RequestBody @Valid SignupRequestDto requestDto){
-        return memberService.signup(requestDto);
+    @RequestMapping(value="/member/signup",method= RequestMethod.POST)
+    public ResponseEntity<?> signup(@RequestBody @Valid SignupRequestDto requestDto){
+        ResponseMessage<?> data = memberService.signup(requestDto);
+        return ResponseEntity.ok().body(data);
     }
 //
     @RequestMapping(value = "/member/login", method = RequestMethod.POST)
-    public ResponseDto<?> login(@RequestBody @Valid LoginRequestDto requestDto, HttpServletResponse response){
-        return memberService.login(requestDto, response);
+    public ResponseEntity<?> login(@RequestBody @Valid LoginRequestDto requestDto, HttpServletResponse response){
+        ResponseMessage<?> data = memberService.login(requestDto, response);
+        return ResponseEntity.ok().body(data);
     }
 //
 //    @RequestMapping(value ="/auth/member/logout", method = RequestMethod.GET)
